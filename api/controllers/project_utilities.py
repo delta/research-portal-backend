24334a6f74855283a08dba61101bb3c21cd76e55
Senthilnathan <mailsenthilnathan2001@gmail.com>
2021-08-31 15:22:21 +0530

    feat(backend): fixes in privileges

--- api/controllers/project_utilities.py
+++ api/controllers/project_utilities.py
@@ -8,8 +8,8 @@ def create_project(name, abstract, paper_link, head, department, aor):
         Helper to create project and assign project user relationship
     """
     try:
-        project_member_privilege = ProjectMemberPrivilege.objects.filter(name = "Admin")
-        if project_member_privilege.exists():
+        # project_member_privilege = ProjectMemberPrivilege.objects.filter(name = "Admin")
+        if head.is_staff:
             project = Project.objects.create(name = name, abstract = abstract, paper_link = paper_link, head = head, department = department, aor = aor)
             ProjectMemberRelationship.objects.create(project = project, user = head, privilege = project_member_privilege[0])
             return True
