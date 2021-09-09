33e72360db141c3c2cc90508b808e7227f4e8ce4
AananthV <aananthv@outlook.com>
2021-09-10 00:15:58 +0530

    feat: Email notifications for projects

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 Backend of the Research Portal of NIT Trichy.
 
 # Local development setup
+
+### Prereqs
+
+1. Run rabbitmq
+
+```
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
+```
+
+2. Run [delta/rabbitmq-smtp-mailer](https://github.com/delta/rabbitmq-smtp-mailer) (optional)
+
 ### With Docker (Recommended)
 
 1. `cp .env.example .env`
@@ -20,4 +31,13 @@ pgAdmin interface is served at localhost:5050 with username admin@admin.com and 
 4. `pip install pre-commit pylint && pre-commit install`
 
 ## To seed departments, privileges from fixtures
+
+```
 python manage.py loaddata api/fixtures/*.json
+```
+
+To seed on development docker container
+
+```
+docker-compose -f docker-compose.dev.yml exec api python manage.py loaddata api/fixtures/*.json
+```
--- api/controllers/project_utilities.py
+++ api/controllers/project_utilities.py
@@ -12,10 +12,28 @@ def create_project(name, abstract, paper_link, head, department, aor):
         if head.is_staff:
             project = Project.objects.create(name = name, abstract = abstract, paper_link = paper_link, head = head, department = department, aor = aor)
             ProjectMemberRelationship.objects.create(project = project, user = head, privilege = project_member_privilege[0])
-            return True
+            return project
         else:
             logger.error("Admin access not found")
             return False
     except Exception as e:
         logger.error(e)
         return False
+
+def get_project_members(project):
+    """
+    Helper function to get members of a given project
+    """
+    try:
+        project_member_relationships = ProjectMemberRelationship.objects.filter(project = project)
+        
+        # Use a set() to get unique users.
+        project_members = list(set(filter(
+            lambda pmr: pmr.user,
+            project_member_relationships
+        )))
+
+        return project_members
+    except Exception as e:
+        logger.error(e)
+        return []
